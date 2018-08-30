f49086791af648188e1557f04607565a27000eff
AlexanderSergan <veteroque@gmail.com>
2018-08-31 01:43:45 +0300

    Angular livereload set up

---
+++ dev-integration-test-app/src/app/app.module.ts
@@ -0,0 +1,14 @@
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import "@livingui/cwc-input/dist/livingui-input";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule],
+  providers: [],
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
+})
+export class AppModule {}
